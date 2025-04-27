e23ce44b85af778f93689700a609106efda4258b
itsgiandolor <gdolor@gbox.adnu.edu.ph>
2025-04-27 22:36:37 +0800

    React.js Homepage
    Created a basic homepage using react.js.
    Added sample post and decorated it with css design.

---
+++ client/src/App.js
@@ -0,0 +1,62 @@
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <header>
+        <a href="" className="logo">MyBlog</a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
+      </header>
+
+      <div className="post">
+        <div className="image">
+          <img src="https://sports.inquirer.net/files/2025/04/IMG_0366_UAAP-Season-87-womens-volleyball-NU-UST_NU-Lady-Bulldogs-celebration-scaled.jpg.avif"></img>
+        </div>
+        <div className="text">
+          <h2>UAAP: NU escapes, forces UST to playoff vs La Salle for bonus</h2>
+          <p className="info">
+            <a className="author">Marlo Cueto</a>
+            <time>2025-27-04 21:04</time>
+          </p>
+          <p className="summary">National University slammed the door shut on University of Santo Tomas, sending a strong message ahead of the UAAP Season 87 womens volleyball Final Four.</p>
+        </div> 
+      </div>
+
+      <div className="post">
+        <div className="image">
+          <img src="https://sports.inquirer.net/files/2025/04/IMG_0366_UAAP-Season-87-womens-volleyball-NU-UST_NU-Lady-Bulldogs-celebration-scaled.jpg.avif"></img>
+        </div>        
+        <div className="text">
+          <h2>UAAP: NU escapes, forces UST to playoff vs La Salle for bonus</h2>
+          <p className="info">
+            <a className="author">Marlo Cueto</a>
+            <time>2025-27-04 21:04</time>
+          </p>
+          <p className="summary">National University slammed the door shut on University of Santo Tomas, sending a strong message ahead of the UAAP Season 87 womens volleyball Final Four.</p>
+        </div>
+              
+      </div>
+
+      <div className="post">
+        <div className="image">
+          <img src="https://sports.inquirer.net/files/2025/04/IMG_0366_UAAP-Season-87-womens-volleyball-NU-UST_NU-Lady-Bulldogs-celebration-scaled.jpg.avif"></img>
+        </div>        
+        <div className="text">
+          <h2>UAAP: NU escapes, forces UST to playoff vs La Salle for bonus</h2>
+          <p className="info">
+            <a className="author">Marlo Cueto</a>
+            <time>2025-27-04 21:04</time>
+          </p>
+          <p className="summary">National University slammed the door shut on University of Santo Tomas, sending a strong message ahead of the UAAP Season 87 womens volleyball Final Four.</p>
+        </div>
+              
+      </div>
+      
+    </main>
+  );
+}
+
+export default App;
