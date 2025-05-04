24b976045fce67a477b5eff0ff78a74691c9ccc6
itsgiandolor <gdolor@gbox.adnu.edu.ph>
2025-05-04 18:24:59 +0800

    Create new post page [Complete]
    Completed the function of creating a new post and uploading the data into the database.

--- client/src/Pages/CreatePost.js
+++ client/src/Pages/CreatePost.js
@@ -1,13 +1,66 @@
+import { useState } from "react";
 import ReactQuill from "react-quill";
 import 'react-quill/dist/quill.snow.css';
 
+const modules = {
+    toolbar: [
+        [{ 'header': [1, 2, false] }],
+        ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+        [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '-2'} ],
+        ['link', 'image'],
+        ['clean']
+    ]
+};
+
+const formats = [
+    'header',
+    'bold', 'italic', 'underline', 'strike', 'blockquote',
+    'list', 'bullet', 'indent',
+    'link', 'image'
+];
+
 export default function CreatePost(){
+    const [title,setTitle] = useState('');
+    const [summary,setSummary] = useState('');
+    const [content,setContent] = useState('');
+    const [files,setFile] = useState('');
+    const [redirect,setRedirect] = useState(false);
+    async function createNewPost(ev){
+        const data = new FormData();
+        data.set('title', title);
+        data.set('summary', summary);
+        data.set('content', content);
+        data.set('file', files[0]);
+        ev.preventDefault();
+        const response = await fetch('http://localhost:4000/post', {
+            method: 'POST',
+            body: data,
+        });
+        if (response.ok){
+            setRedirect(true);
+        }
+    }
+
+    if(redirect){
+        return <Navigate to={'/'} />
+    }
     return (
-        <form>
-            <input type="title" placeholder={'Title'}/>
-            <input type="summary" placeholder={'Summary'} />
-            <input type="file"/>
-            <ReactQuill />
+        <form onSubmit={createNewPost}>
+            <input type="title" 
+                    placeholder={'Title'} 
+                    value={title} 
+                    onChange={ev => setTitle(ev.target.value)}/>
+            <input type="summary" 
+                    placeholder={'Summary'} 
+                    value={summary} 
+                    onChange={ev => setSummary(ev.target.value)}/>
+            <input type="file" 
+                    onChange={ev => setFile(ev.target.files)}/>
+            <ReactQuill value={content} 
+                    onChange={newValue => setContent(newValue)}
+                    modules={modules} 
+                    formats={formats}/>
+            <button style={{marginTop: '5px'}}>Publish Post</button>
         </form>
     );
 }
