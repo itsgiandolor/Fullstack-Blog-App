6e98899d5d8758e7c3c971c5439af8be21e68cff
itsgiandolor <gdolor@gbox.adnu.edu.ph>
2025-05-04 15:41:36 +0800

    Authentication done
    Finished authentication, checking if logged in, added logout functionality and usercontext. Currently in the process of creating a "create new post page"

--- api/index.js
+++ api/index.js
@@ -5,12 +5,14 @@ const User = require('./models/User');
 const bcrypt = require('bcryptjs');
 const app = express();
 const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 
 const salt = bcrypt.genSaltSync(10);
 const secret = 'asdfghjkloiuytre123456';
 
 app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
 app.use(express.json());
+app.use(cookieParser());
 
 mongoose.connect('mongodb+srv://gdolor:gdolor@cluster0.upvitmk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 
@@ -36,12 +38,27 @@ app.post('/login', async (req,res) => {
         // logged in
         jwt.sign({username, id:userDoc._id}, secret, {}, (err, token) => {
             if (err) throw err;
-            res.cookie('token', token).json('ok');
+            res.cookie('token', token).json({
+                id:userDoc._id,
+                username,
+            });
         });
     } else {
         // not logged in
         res.status(400).json('wrong credentials');
     }
 });
+
+app.get('/profile', (req,res) => {
+    const {token} = req.cookies;
+    jwt.verify(token, secret, {}, (err, info) =>{
+        if (err) throw err;
+        res.json(info);
+    });
+});
+
+app.post('/logout', (req,res) => {
+    res.cookie('token', '').json('ok');
+})
 
 app.listen(4000);
--- client/src/App.js
+++ client/src/App.js
@@ -6,17 +6,23 @@ import Layout from './Layout';
 import IndexPage from './Pages/IndexPage';
 import LoginPage from './Pages/LoginPage';
 import RegisterPage from './Pages/RegisterPage';
+import { UserContextProvider } from './UserContext';
+import CreatePost from './Pages/CreatePost';
 
 function App() {
   return (
-    <Routes>
-      <Route path ="/" element={<Layout />}>
-        <Route index element={<IndexPage />} />
+    <UserContextProvider>
+      <Routes>
+        <Route path ="/" element={<Layout />}>
+          <Route index element={<IndexPage />} />
 
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-      </Route>
-    </Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/create" element={<CreatePost />} />
+        </Route>
+      </Routes>
+    </UserContextProvider>
+    
 
     
   );
