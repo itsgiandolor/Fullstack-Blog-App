ad960c2b2ab28857e87a5e3a0e7589932fb5b5d8
itsgiandolor <gdolor@gbox.adnu.edu.ph>
2025-05-04 11:17:48 +0800

    Authentication
    Added authentication in the backend. Users can now register and then log in with their account. Added password encryption, and logging in logic.

--- api/index.js
+++ api/index.js
@@ -1,9 +1,47 @@
 const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const User = require('./models/User');
+const bcrypt = require('bcryptjs');
 const app = express();
+const jwt = require('jsonwebtoken');
+
+const salt = bcrypt.genSaltSync(10);
+const secret = 'asdfghjkloiuytre123456';
+
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
+app.use(express.json());
+
+mongoose.connect('mongodb+srv://gdolor:gdolor@cluster0.upvitmk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 
 app.post('/register', (req, res) =>{
-    res.json('test ok');
+    const {username,password} = req.body;
+    try{
+        const userDoc = User.create({
+            username, 
+            password:bcrypt.hashSync(password,salt)});
+        res.json(userDoc);
+    } catch(e){
+        res.status(400).json(e);
+    }
+    
+    
+});
 
-})
+app.post('/login', async (req,res) => {
+    const {username,password} = req.body;
+    const userDoc = await User.findOne({username});
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    if (passOk) {
+        // logged in
+        jwt.sign({username, id:userDoc._id}, secret, {}, (err, token) => {
+            if (err) throw err;
+            res.cookie('token', token).json('ok');
+        });
+    } else {
+        // not logged in
+        res.status(400).json('wrong credentials');
+    }
+});
 
 app.listen(4000);
